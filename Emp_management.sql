52b2db195b4d407e11668658e537a92a052a7a2f
roshitkr <153702127+roshitkr@users.noreply.github.com>
2024-09-05 08:56:06 +0530

    Add files via upload

---
+++ Emp_management.sql
@@ -0,0 +1,9 @@
+Create database Emp_management;
+use Emp_management;
+create table Emp_management(
+     id int primary key,
+     Name varchar(20),
+     Age int,
+     salary decimal(10,2)
+);
+select *from Emp_management;
